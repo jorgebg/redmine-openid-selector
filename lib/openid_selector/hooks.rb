aae7a599dc4b846a9e020cd3726a89fb494e8724
Michał Rus <m@michalrus.com>
2013-07-06 21:24:33 +0000

    Move OpenID box to the top, change prompt

--- lib/openid_selector/hooks.rb
+++ lib/openid_selector/hooks.rb
@@ -1,6 +1,6 @@
 module OpenIDSelector
   class Hooks < Redmine::Hook::ViewListener
-    render_on :view_account_login_bottom,
-              :partial => 'view_account_login_bottom'
+    render_on :view_account_login_top,
+              :partial => 'view_account_login_top'
   end
 end
